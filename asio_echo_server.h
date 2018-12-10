ef3c7267cd868c9de2b704eb8baf24fb15e31e40
權少 <ljqican@163.com>
2018-12-11 00:03:39 +0800

    清除無用聲明

--- asio_echo_server.h
+++ asio_echo_server.h
@@ -6,8 +6,6 @@ void write_handler(const error_code_t &ec,
 	shared_ptr<session_t> session,
 	io_context_t &io_context);
 
-bool async_write(shared_ptr<session_t> session, io_context_t &io_context);
-
 void read_handler(const error_code_t &ec,
 	std::size_t size,
 	shared_ptr<session_t> session,
@@ -17,5 +15,3 @@ void accept_handler(const error_code_t &ec,
 	shared_ptr<session_t> session,
 	io_context_t &io_context,
 	acceptor_t &acceptor);
-
-void async_read(shared_ptr<session_t> session, io_context_t &io_context);
