b05b5be72117221c5df1b04f8d9709eef5af8e0c
權少 <ljqican@163.com>
2019-07-09 21:47:59 +0800

    使用單例保存配置

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,21 @@
 #include "session.hpp"
 #include <ctime>
 
+struct config_t
+{
+    std::size_t read_size = 1;
+    std::size_t write_size = 1;
+    std::size_t timer_interval = 1;
+    std::size_t timeout = 1;
+};
+
+static config_t& get_config()
+{
+    static config_t instance;
+    return instance;
+}
+
+
 std::map<std::string, std::shared_ptr<session_t>> session_t::manager;
 
 uint64_t get_unix_time()
@@ -45,12 +60,12 @@ void on_write(shared_ptr<session_t> session, error_code_t ec)
     if (write_buffer.offset() == write_buffer.size())
     {
         write_buffer.clear();
-        async_read(session, 1, on_read);
+        async_read(session, get_config().read_size, on_read);
     }
     else
     {
         LOG(info);
-        async_write(session, 1, on_write);
+        async_write(session, get_config().write_size, on_write);
     }
 }
 
@@ -87,11 +102,11 @@ void on_read(shared_ptr<session_t> session, error_code_t ec)
         //LOG(info, "hex:{0} {1} {2}", hex, write_buffer.offset(), write_buffer.size());
         //assert(false);
         read_buffer.clear();
-        async_write(session, 1, on_write);
+        async_write(session, get_config().write_size, on_write);
     }
     else
     {
-        async_read(session, 1, on_read);
+        async_read(session, get_config().read_size, on_read);
     }
 }
 
@@ -105,14 +120,14 @@ void on_accept(shared_ptr<session_t> session, error_code_t ec)
     }
     session_t::manager[session->id] = session;
     session->unix_time = get_unix_time();
-    async_read(session, 1, on_read);
+    async_read(session, get_config().read_size, on_read);
     auto new_session = make_shared<session_t>(session->io_context, session->acceptor);
     async_accept(new_session, on_accept);
 }
 
 void on_wait(error_code_t ec, shared_ptr<steady_timer_t> timer)
 {
-    LOG(info, "time out");
+    LOG(info, "session size:{0}", session_t::manager.size());
     for (auto i = session_t::manager.begin(); i != session_t::manager.end();)
     {
         auto session = i->second;
@@ -120,8 +135,9 @@ void on_wait(error_code_t ec, shared_ptr<steady_timer_t> timer)
         LOG(info, "session:{0}", i->first);
         LOG(info, "unix_time:{0}", unix_time);
         auto now_time = get_unix_time();
-        LOG(info, now_time - unix_time);
-        if (now_time - unix_time > 5)
+        LOG(info, "time:{0}", now_time - unix_time);
+        LOG(info, "timeout:{0}", get_config().timeout);
+        if (now_time - unix_time > get_config().timeout)
         {
             session->socket.close();
             auto current = i;
@@ -144,6 +160,11 @@ void on_wait(error_code_t ec, shared_ptr<steady_timer_t> timer)
 
 int main()
 {
+    auto& config = get_config();
+    config.read_size = 2;
+    config.write_size = 2;
+    config.timer_interval = 3;
+    config.timeout = 10;
     spdlog::easy::init();
     auto io_context = make_shared<io_context_t>();
     auto acceptor = make_shared<acceptor_t>(
@@ -154,7 +175,7 @@ int main()
         async_accept(session, on_accept);
     }
     auto timer = make_shared<steady_timer_t>(*io_context);
-    timer->expires_from_now(chrono::seconds(3));
+    timer->expires_from_now(chrono::seconds(config.timer_interval));
     timer->async_wait(
         [timer](error_code_t ec)
     {
